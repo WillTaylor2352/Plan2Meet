d2abbacda4f5a0bf0373076c093ee7cbb16d1159
M-Bischoff <93992306+M-Bischoff@users.noreply.github.com>
2022-06-24 10:10:46 -0400

    Added dto package, created contacts.kt dto following class diagram

---
+++ app/src/main/java/com/example/plan2meet/dto/Contacts.kt
@@ -0,0 +1,6 @@
+package com.example.plan2meet.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class Contacts (@SerializedName("name") var name : String = "", @SerializedName("email") var email : String = "", @SerializedName("phone") var phone : Int = 0){
+}
