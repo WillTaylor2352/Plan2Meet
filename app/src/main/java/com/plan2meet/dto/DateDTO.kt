28610b6d5539565c8beea1d6feef3419164d69f4
Will Taylor <willtaylor2352@gmail.com>
2022-07-08 15:06:52 -0400

    Added DTO's and updated others

---
+++ app/src/main/java/com/plan2meet/dto/DateDTO.kt
@@ -0,0 +1,11 @@
+package com.plan2meet.dto
+
+import java.time.LocalDate
+
+class DateDTO (
+    val currentDate     : LocalDate,
+    var events          : Array<EventDTO>
+        )
+{
+
+}
--- app/src/main/java/com/plan2meet/ui/MainActivity.kt
+++ app/src/main/java/com/plan2meet/ui/MainActivity.kt
@@ -3,10 +3,14 @@ package com.plan2meet.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.plan2meet.R
+import com.plan2meet.dto.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val event = EventDTO();
+        print(event.toString())
         setContentView(R.layout.activity_main)
+
     }
 }
