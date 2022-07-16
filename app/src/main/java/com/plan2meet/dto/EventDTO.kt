cc6d66be49163302ac1d51855dea6bf723880d34
sharmrb <57459468+sharmrb@users.noreply.github.com>
2022-07-16 15:51:38 -0400

    Created GroupListProvider class. Need to create an activity to provide list of contacts rather than doing it in the main activity.

--- app/src/main/java/com/plan2meet/dto/EventDTO.kt
+++ app/src/main/java/com/plan2meet/dto/EventDTO.kt
@@ -1,11 +1,12 @@
 package com.plan2meet.dto
 
 import android.os.Build
+import android.os.Build.VERSION_CODES
 import androidx.annotation.RequiresApi
 import java.time.LocalDateTime
 import java.time.LocalDateTime.now
 
-@RequiresApi(Build.VERSION_CODES.26)
+@RequiresApi(VERSION_CODES.26)
 class EventDTO (
     val startTime: LocalDateTime = LocalDateTime.now(),
     val endTime: LocalDateTime = LocalDateTime.now()
