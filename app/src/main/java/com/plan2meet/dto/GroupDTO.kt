3db5d5570f6b62009dad8a24a1fcd0ad1af13454
Robert Champion <champirr@mail.uc.edu>
2022-07-15 20:37:09 -0400

    Update GroupDTO.kt

--- app/src/main/java/com/plan2meet/dto/GroupDTO.kt
+++ app/src/main/java/com/plan2meet/dto/GroupDTO.kt
@@ -2,6 +2,7 @@ package com.plan2meet.dto
 
 class GroupDTO (
     var calendarDTO    : CalendarDTO?,
+    //make sure to use your variables in your code to lessen technical debt.
     var contacts    : Array<ContactDTO>,
     val configFile  : String = "")
 {
