77d992d26f9a88da7dae83d0c160e572b7463226
Chloe Motz <finnickodair11@gmail.com>
2022-07-16 10:36:30 -0400

    Added Firebase and updated dependencies to newest versions

--- app/src/main/java/com/plan2meet/ui/MainViewModel.kt
+++ app/src/main/java/com/plan2meet/ui/MainViewModel.kt
@@ -1,4 +1,16 @@
 package com.plan2meet.ui
 
-class MainViewModel {
+import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
+import com.google.firebase.storage.FirebaseStorage
+
+class MainViewModel : ViewModel() {
+
+    //firebase
+    private var firestore : FirebaseFirestore = FirebaseFirestore.getInstance()
+    private var storageReference = FirebaseStorage.getInstance().reference
+    init {
+        firestore.firestoreSettings = FirebaseFirestoreSettings.Builder().build()
+    }
 }
