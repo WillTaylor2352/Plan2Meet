668db62f9209811b8bcb5a7405b3d73b8fd2c14b
sharmrb <57459468+sharmrb@users.noreply.github.com>
2022-07-16 16:24:02 -0400

    Added First Id to store event in db. created blank Updatedb function

--- app/src/main/java/com/plan2meet/dto/EventDTO.kt
+++ app/src/main/java/com/plan2meet/dto/EventDTO.kt
@@ -1,10 +1,13 @@
 package com.plan2meet.dto
 
+import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Build.VERSION_CODES
 import androidx.annotation.RequiresApi
+import androidx.room.PrimaryKey
 import java.time.LocalDateTime
 import java.time.LocalDateTime.now
+
 
 @RequiresApi(VERSION_CODES.26)
 class EventDTO (
@@ -13,5 +16,13 @@ class EventDTO (
         ) {
     override fun toString(): String {
         return startTime.toString()
+    }
+
+    @JvmField
+    @PrimaryKey(autoGenerate = true)
+    var id: Long = 0
+
+    fun UpdateDb(){
+
     }
 }
