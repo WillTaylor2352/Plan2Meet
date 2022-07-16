bedc24dee361d3127b1e6fef3701497c57da8ecb
Robert Champion <champirr@mail.uc.edu>
2022-07-15 20:36:37 -0400

    Update ContactDTO.kt
    variable should never start with uppercase letters

--- app/src/main/java/com/plan2meet/dto/ContactDTO.kt
+++ app/src/main/java/com/plan2meet/dto/ContactDTO.kt
@@ -8,9 +8,13 @@ class ContactDTO (
     @SerializedName("email") val email     : String = "",
     @SerializedName("phone") val phone     : String = "", // this value cannot be an Integer due to the size of "Int"
     @SerializedName("favorite") var favorite : Boolean = false)
+
+
 {
     @Override
     override fun toString(): String {
-        return "My name is $firstName $lastName, my e-mail is: $email and my phone # is: $phone"
+        //use variable to help clean up return code
+        val contact = "My name is $firstName $lastName, my e-mail is: $email and my phone # is: $phone"
+        return contact
     }
 }
